b0130fff8173e73c5ea6efe0889d10e8a74ac00d
risingstaruj <risingstaruj@naver.com>
2023-03-26 20:07:22 +0900

    로그인 회원가입 폼 수정

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Topbar from './Topbar.js';
+import HStyles from './Home.module.css';
 
 function Home(){
 
 
     return(
     <>
+    
     <Topbar />
-
+    <div className={`${HStyles.LoginForm}`}>
+        <input className={`${HStyles.Input}`}></input>
+        <input className={`${HStyles.Input}`}></input>
+        <br />
+        <button className={`${HStyles.Button}`}>로그인</button>
+        <button className={`${HStyles.Button}`}>회원가입</button>
+    </div>
     </>
     );
 
