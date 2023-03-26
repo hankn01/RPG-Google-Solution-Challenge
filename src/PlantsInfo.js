1f4a9d088ad7cf484c4f0acdd34b9e185df2d878
risingstaruj <risingstaruj@naver.com>
2023-03-27 00:02:08 +0900

    식물 이름 페이지 개정

--- src/PlantsInfo.js
+++ src/PlantsInfo.js
@@ -7,8 +7,18 @@ function PlantsInfo() {
     return(
         <>
             <Topbar />
-            <div className={`${PIStyles.PlantsTile}`}></div>
-            <div className={`${PIStyles.PlantsInnerTile}`}></div>
+            <div className={`${PIStyles.PlantsTile}`}>
+                <div className={`${PIStyles.PlantsTitleCaption}`}>
+                    식물 이름
+                </div>
+
+            </div>
+            <div className={`${PIStyles.PlantsInnerTile}`}>
+                <div className={`${PIStyles.InnerTileText}`}># 물주기</div>
+                <div className={`${PIStyles.InnerTileText}`}># 환기</div>
+                <div className={`${PIStyles.InnerTileText}`}># 일조량</div>
+                <div className={`${PIStyles.InnerTileText}`}># 가지치기</div>
+            </div>
 
 
             
